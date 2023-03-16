fff435eeadb962fad211953f5685fa3cb6e75e68
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-16 10:08:24 -0600

    Completes lab

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,23 @@
 class Student:
-    pass
+    def hello(self, phrase=""):
+        print(f"Hey there! I'm so excited to learn stuff.\n{phrase}" if phrase != "" else "Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self, phrase="Pick me!"):
+        print(phrase)
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for i in range(10):
+            super().raise_hand()
+
+
+stud=Student()
+stud.hello()
+stud.raise_hand()
+
+chatty=ChattyStudent()
+chatty.hello()    
+chatty.raise_hand()
